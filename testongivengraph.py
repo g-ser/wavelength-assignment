b39bbe23a1d4dc838303f58fcf4d6bfc25d9007d
giorgos <gorgser@hotmail.com>
2016-02-17 02:56:07 +0100

    script that implements the Illustrative Example

--- testongivengraph.py
+++ testongivengraph.py
@@ -1,93 +1,78 @@
 #!/usr/bin/env python
 #This python script was written by Georgios Serafeim
 import assignwavelengths as assignwl 
-import networkx as nx
-"""
-Create an empty graph
-"""
-myGraph=nx.Graph() 
 
 """
-Add 11 nodes to the graph
+Create an empty graph with 11 nodes
 """
-myGraph.add_nodes_from(range(1,12))
+fiber_network=assignwl.createGraph(11)
 
 """
-add edges with weights to the graph 1st & 2nd numbers of each tuple represent 
+Add edges with weights to the graph 1st & 2nd numbers of each tuple represent 
 the nodes, 3rd number of the tuple represents the weight. Weights will be later 
 used as input to the shortes path algorithm
 """
 edges=[(1,2,2),(2,3,1),(2,9,2),(2,4,3),(4,6,2),(4,8,4),(4,10,1),\
         (4,5,3),(3,10,2),(3,5,4),(3,7,2),(5,8,2),(5,11,1)]
 
-for i in edges:
-    myGraph.add_edge(i[0],i[1], weight=i[2])
-
-assignwl.printGraph(myGraph)
-"""
-define some routes
-1st number of the tuple represents the source
-2nd number represents the destination
-"""
-routes=[(9,11),(11,6),(6,7),(9,7),(1,6),(7,1),(11,8)]
+assignwl.addEdges(fiber_network,edges)
 
 """
-Assign a number to each of the routes above. The role of the number is just 
-to distinguish between the different routes (it is also used throughout the 
-construction of the auxiliary graph)
+Print the graph
 """
-routes={i+1: routes[i] for i in range(0,len(routes))}
+assignwl.printGraph(fiber_network, "blue", "Given fiber network")
 
 """
-Run the shortest path algorithm given the above routes (weights are considered)
-(The method shortest_path which implements the shortest path algorithm and is 
-included in the NetworkX package will be used)
+Define some lightpaths. The key of the dictionary represents the number of the lightpath
+The role of this number is just to distinguish between the different lightpaths 
+(it is also used throughout the construction of the auxiliary graph)
+1st number of each tuple represents the source node
+2nd number represents the destination node
 """
-followed_path={}
-for key, value in routes.iteritems():
-    z=nx.shortest_path(myGraph,source=value[0],target=value[1], weight="weight")
-    followed_path.update({key:tuple(z)})
+lightpaths={1: (9, 11), 2: (11, 6), 3: (6, 7), 4: (9, 7), 5: (1, 6), 6: (7, 1), 7: (11, 8)}
 
-print followed_path
 """
-Construct the auxiliary graph
+Construct the auxiliary graph. In order to construct the auxiliary graph, 
+the route of each of the lightpaths defined above is found using the shortest 
+path algorithm which then is used as input to the function that makes the auxiliary 
+graph
 """
-myAuxiliaryGraph=assignwl.constructAuxGraph(followed_path)
-assignwl.printGraph(myAuxiliaryGraph)
+myAuxiliaryGraph=assignwl.constructAuxGraph(assignwl.shortestPath(fiber_network, lightpaths))
+assignwl.printGraph(myAuxiliaryGraph,"blue", "Auxiliary Graph")
 
 """
 Estimate the colors that would be needed to color the graph in degree order
 """
-
 assignwl.estimateColorsDegreeOrder(myAuxiliaryGraph)
 
 """
 Perform coloring of the graph (auxiliary graph) in degree order 
 """
+assignwl.printGraph(myAuxiliaryGraph,assignwl.coloringGraph(myAuxiliaryGraph,"degree_order")\
+	, "Coloring in degree order")
 
-assignwl.printGraph(myAuxiliaryGraph,assignwl.coloringGraph(myAuxiliaryGraph,"degree_order")[1])
 """
 Estimate the number of the colors needed in case the graph is colored 
 in alternate order
 """
-
 assignwl.estimateColorsAlternateOrder(myAuxiliaryGraph)
 
 """
 Perform coloring in alternate order
 """
-assignwl.printGraph(myAuxiliaryGraph,assignwl.coloringGraph(myAuxiliaryGraph,"alternate_order")[1])
+assignwl.printGraph(myAuxiliaryGraph,assignwl.coloringGraph(myAuxiliaryGraph,"alternate_order"),\
+ "Coloring in alternate order")
 
 """
 Color the predefined routes of the initial graph based on the results 
 of the coloring algorithm in degree order
 """
-assignwl.drawColoredPathsOnGivenGraph(myGraph, myAuxiliaryGraph, followed_path, "degree_order")
-
+assignwl.drawColoredPathsOnGivenGraph(fiber_network, myAuxiliaryGraph, assignwl.shortestPath(fiber_network,\
+ lightpaths), "degree_order")
 
 """
 Color the predefined routes of the initial graph based on the results 
 of the coloring algorithm in alternate order
 """
-assignwl.drawColoredPathsOnGivenGraph(myGraph, myAuxiliaryGraph, followed_path, "alternate_order")
-
+assignwl.drawColoredPathsOnGivenGraph(fiber_network, myAuxiliaryGraph, assignwl.shortestPath(fiber_network,\
+ lightpaths), "alternate_order")
